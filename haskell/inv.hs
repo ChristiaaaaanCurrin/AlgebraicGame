872d406d69511953f4ba8de87ab4c4aa3306f93b
Christiaan van de Sande <christiaanvandesande@Christiaans-MacBook-Air.local>
2020-11-17 08:15:01 -0700

    I really have it working now...

--- haskell/inv.hs
+++ haskell/inv.hs
@@ -6,11 +6,11 @@
 
 module Inv  where
 
-infixr %
+infixr 8 %
 
 class Inv m where
-  (%) :: m -> m -> m
-  inv :: m -> m
+  (%)  :: m -> m -> m
+  inv  :: m -> m
   pass :: m
 
 instance Num a => Inv a where
@@ -19,19 +19,19 @@ instance Num a => Inv a where
   pass = 0
 
 instance (Inv b, Inv d) => Inv (b, d) where 
-  (%) (y1, w1) (y2, w2) = (y1 % y2, w1 % w2)
-  inv (y, w) = (inv y, inv w)
+  (%)  (y1, w1) (y2, w2) = (y1 % y2, w1 % w2)
+  inv  (y, w) = (inv y, inv w)
   pass = (pass, pass)
 
 instance (Inv b, Inv c) => Inv (Either b c) where
-  (%) (Left  y1) (Left  y2) = Left  $ y1 % y2
-  (%) (Right y1) (Right y2) = Right $ y1 % y2
-  inv (Left  y) = Left  $ inv y
-  inv (Right y) = Right $ inv y
-  pass = Left pass
+  (%)  (Left  y1) (Left  y2) = Left  $ y1 % y2
+  (%)  (Right y1) (Right y2) = Right $ y1 % y2
+  inv  (Left  y) = Left  $ inv  y
+  inv  (Right y) = Right $ inv  y
+  pass = Left  pass
 
 instance (Inv b) => Inv [b] where
   (%) = (++)
-  inv ys = map inv $ reverse ys
-  pass = []
+  inv  ys = map inv $ reverse ys
+  pass = pure pass
  
--- haskell/rule.hs
+++ haskell/rule.hs
@@ -7,31 +7,48 @@ module Rule where
 import Move
 import Inv
 
+
+class InvR r where
+  (/%)  :: r -> r -> r
+  invR  :: r -> r
+  passR :: r
+
 type Rule a b = a -> [b]
-instance Inv b => Inv (Rule a b) where
-  (r % s) x = [z % y | z <- r x, y <- s x]
-  (inv r) x = map inv $ r x
-  pass x = [pass]
+instance Inv b => InvR (Rule a b) where
+  (r /% s) x = [z % y | z <- r x, y <- s x]
+  invR r x = map inv $ r x
+  passR x = [pass]
 
 gate :: Inv b => (a -> Bool) -> Rule a b
 gate f x
-  | f x = [pass]
+  | f x = pure pass
   | otherwise = []
 
+infixl 9 /:
+(/:) :: Inv b => Rule a b -> Int -> Rule a b
+r /: 1 = r
+r /: n = r /% r /: (n - 1)
+
+infixr 8 /.
+(/.) :: Rule a b -> Rule a c -> Rule a [Either b c]
+(r /. s) x = [[Left y, Right z] | y <- r x, z <- s x]
+
+infixr 7 /*
 (/*) :: Rule a b -> Rule c d -> Rule (a, c) (b, d)
 (r /* s) (x, z) = [(y, w) | y <- r x, w <- s z]
 
-(/./) :: Rule a b -> Rule a c -> Rule a [Either b c]
-(r /./ s) x = [[Left y, Right z] | y <- r x, z <- s x]
-
-(//) :: Rule a b -> Rule a b -> Rule a b
-(r // s) x = (r x) ++ (s x)
-
-(/-/) :: Rule a b -> Rule a c -> Rule a (Either b c)
-(r /-/ s) x = (map Left $ r x) ++ (map Right $ s x)
-
+infixl 6 /\
 (/\) :: (Eq b) => Rule a b -> Rule a b -> Rule a b
 (r /\ s) x = [y | y <- s x, y `elem` r x]
 
+infixl 5 //
+(//) :: Rule a b -> Rule a b -> Rule a b
+(r // s) x = (r x) ++ (s x)
+
+infixr 5 /-/
+(/-/) :: Rule a b -> Rule a c -> Rule a (Either b c)
+(r /-/ s) x = (map Left $ r x) ++ (map Right $ s x)
+
+infixl 4 /+
 r /+ s = (r /* pass) /-/ (pass /* s) 
 
